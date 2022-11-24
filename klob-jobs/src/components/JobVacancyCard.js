61379996fb072b2d3b7057d449876c6698baaa30
Aji Purnomo <semlakune@gmail.com>
2022-11-24 17:24:02 +0700

    update: release 2 done

--- klob-jobs/src/components/JobVacancyCard.js
+++ klob-jobs/src/components/JobVacancyCard.js
@@ -1,8 +1,14 @@
+import {useDispatch} from "react-redux";
+import {jobVacancyById} from "../store/actions/actionJobVacancy";
+import {useNavigate} from "react-router-dom";
 
 export default function JobVacancyCard(props) {
     const {jobVacancy} = props;
+    const navigate = useNavigate()
 
-    const idRupiah = Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR", });
+    const jobVacancyDetailHandler = (jobVacancyCode) => {
+        navigate(`/detail-lowongan-perkerjaan/${jobVacancyCode}`)
+    }
     return (
         <div className={"w-96 rounded overflow-hidden shadow-lg m-5"}>
             <div className={"flex items-center justify-center p-10"}>
@@ -17,10 +23,10 @@ export default function JobVacancyCard(props) {
                 </div>
                 <div className={"grid grid-cols-2 mb-2"}>
                     <h1>Gaji</h1>
-                    <h1 className={"bg-stone-500 p-0.5 text-center rounded text-white"}>{idRupiah.format(jobVacancy.salaryFrom)} - {idRupiah.format(jobVacancy.salaryTo)}</h1>
+                    <h1 className={"bg-stone-500 p-0.5 text-center rounded text-white"}>{jobVacancy.salaryFrom} - {jobVacancy.salaryTo}</h1>
                 </div>
                 <div className={"grid grid-cols-2 mb-2"}>
-                    <button className={"text-blue-600 text-left"}>Baca Detail</button>
+                    <button onClick={() => jobVacancyDetailHandler(jobVacancy.jobVacancyCode)} type={"button"} className={"text-blue-600 text-left"}>Baca Detail</button>
                     <h1 className={"text-right"}>{jobVacancy.postedDate}</h1>
                 </div>
                 <button className={"text-white bg-green-800 w-full p-2 rounded-xl mt-5 hover:bg-green-600"}>KIRIM LAMARAN</button>
